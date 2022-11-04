f388922a23bc07c1f93d01a686f8d0f8268b67d2
cryscan <cryscan@umich.edu>
2022-11-04 20:16:24 +0000

    Also scale prepass textures correctly.

--- src/prepass.rs
+++ src/prepass.rs
@@ -356,9 +356,10 @@ fn prepass_textures_system(
 ) {
     for (entity, camera) in &queries.p0() {
         if let Some(size) = camera.physical_target_size() {
+            let scale = 1.0 / config.upscale_ratio.max(1.0);
             let size = Extent3d {
-                width: (size.x as f32 / config.upscale_ratio) as u32,
-                height: (size.y as f32 / config.upscale_ratio) as u32,
+                width: (size.x as f32 * scale).ceil() as u32,
+                height: (size.y as f32 * scale).ceil() as u32,
                 depth_or_array_layers: 1,
             };
             let texture_usage = TextureUsages::COPY_DST
