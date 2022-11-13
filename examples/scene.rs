7dd74b48e10c9d52504869431d09ef6288b548fb
cryscan <cryscan@umich.edu>
2022-11-12 21:02:04 -0500

    Make examples compile.

--- examples/scene.rs
+++ examples/scene.rs
@@ -1,40 +1,21 @@
-use bevy::{
-    input::mouse::{MouseMotion, MouseScrollUnit, MouseWheel},
-    prelude::*,
-    render::camera::CameraRenderGraph,
-};
+use bevy::{prelude::*, render::camera::CameraRenderGraph};
 use bevy_hikari::prelude::*;
-use bevy_inspector_egui::WorldInspectorPlugin;
-use bevy_mod_raycast::{
-    DefaultRaycastingPlugin, Intersection, RayCastMesh, RayCastMethod, RayCastSource, RaycastSystem,
-};
-use smooth_bevy_cameras::{
-    controllers::orbit::{
-        ControlEvent, OrbitCameraBundle, OrbitCameraController, OrbitCameraPlugin,
-    },
-    LookTransformPlugin,
-};
 use std::f32::consts::PI;
 
 fn main() {
     App::new()
-        // .insert_resource(WindowDescriptor {
-        //     width: 400.,
-        //     height: 300.,
-        //     ..Default::default()
-        // })
         .add_plugins(DefaultPlugins)
-        .add_plugin(WorldInspectorPlugin::new())
-        .add_plugin(LookTransformPlugin)
-        .add_plugin(OrbitCameraPlugin::new(false))
-        .add_plugin(DefaultRaycastingPlugin::<RaycastSet>::default())
+        // .add_plugin(WorldInspectorPlugin::new())
+        // .add_plugin(LookTransformPlugin)
+        // .add_plugin(OrbitCameraPlugin::new(false))
+        // .add_plugin(DefaultRaycastingPlugin::<RaycastSet>::default())
         .add_plugin(HikariPlugin)
         .add_startup_system(setup)
-        .add_system(camera_input_map)
-        .add_system_to_stage(
-            CoreStage::First,
-            control_directional_light.before(RaycastSystem::BuildRays::<RaycastSet>),
-        )
+        // .add_system(camera_input_map)
+        // .add_system_to_stage(
+        //     CoreStage::First,
+        //     control_directional_light.before(RaycastSystem::BuildRays::<RaycastSet>),
+        // )
         .run();
 }
 
@@ -49,8 +30,8 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn((
+        PbrBundle {
             mesh: meshes.add(shape::Plane::default().into()),
             material: materials.add(StandardMaterial {
                 base_color: Color::rgb(0.8, 0.7, 0.6).into(),
@@ -63,18 +44,19 @@ fn setup(
                 ..Default::default()
             },
             ..Default::default()
-        })
-        .insert(RayCastMesh::<RaycastSet>::default());
+        },
+        // RayCastMesh::<RaycastSet>::default(),
+    ));
 
-    commands.spawn_bundle(SceneBundle {
+    commands.spawn(SceneBundle {
         scene: asset_server.load("models/scene.gltf#Scene0"),
         transform: Transform::default(),
         ..default()
     });
 
     // Sphere
-    commands
-        .spawn_bundle(PbrBundle {
+    commands.spawn((
+        PbrBundle {
             mesh: meshes.add(Mesh::from(shape::UVSphere {
                 radius: 0.5,
                 ..Default::default()
@@ -87,12 +69,13 @@ fn setup(
             }),
             transform: Transform::from_xyz(2.0, 2.0, 0.0),
             ..Default::default()
-        })
-        .insert(EmissiveSphere)
-        .insert(Name::new("Emissive Sphere"));
+        },
+        EmissiveSphere,
+        Name::new("Emissive Sphere"),
+    ));
 
     // Only directional light is supported
-    commands.spawn_bundle(DirectionalLightBundle {
+    commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             illuminance: 100000.0,
             ..Default::default()
@@ -106,102 +89,103 @@ fn setup(
     });
 
     // Camera
-    commands
-        .spawn_bundle(Camera3dBundle {
+    commands.spawn((
+        Camera3dBundle {
             camera_render_graph: CameraRenderGraph::new(bevy_hikari::graph::NAME),
             transform: Transform::from_xyz(-10.0, 2.5, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
-        })
-        .insert_bundle(OrbitCameraBundle::new(
-            OrbitCameraController::default(),
-            Vec3::new(-50.0, 25.0, 100.0),
-            Vec3::new(0., 0., 0.),
-        ))
-        .insert(RayCastSource::<RaycastSet>::default());
+        },
+        // OrbitCameraBundle::new(
+        //     OrbitCameraController::default(),
+        //     Vec3::new(-50.0, 25.0, 100.0),
+        //     Vec3::new(0., 0., 0.),
+        // ),
+        // RayCastSource::<RaycastSet>::default(),
+    ));
 }
 
-pub fn camera_input_map(
-    mut events: EventWriter<ControlEvent>,
-    mut mouse_wheel_reader: EventReader<MouseWheel>,
-    mut mouse_motion_events: EventReader<MouseMotion>,
-    mouse_buttons: Res<Input<MouseButton>>,
-    controllers: Query<&OrbitCameraController>,
-) {
-    // Can only control one camera at a time.
-    let controller = if let Some(controller) = controllers.iter().next() {
-        controller
-    } else {
-        return;
-    };
-    let OrbitCameraController {
-        enabled,
-        mouse_rotate_sensitivity,
-        mouse_translate_sensitivity,
-        mouse_wheel_zoom_sensitivity,
-        pixels_per_line,
-        ..
-    } = *controller;
+// pub fn camera_input_map(
+//     mut events: EventWriter<ControlEvent>,
+//     mut mouse_wheel_reader: EventReader<MouseWheel>,
+//     mut mouse_motion_events: EventReader<MouseMotion>,
+//     mouse_buttons: Res<Input<MouseButton>>,
+//     controllers: Query<&OrbitCameraController>,
+// ) {
+//     // Can only control one camera at a time.
+//     let controller = if let Some(controller) = controllers.iter().next() {
+//         controller
+//     } else {
+//         return;
+//     };
+//     let OrbitCameraController {
+//         enabled,
+//         mouse_rotate_sensitivity,
+//         mouse_translate_sensitivity,
+//         mouse_wheel_zoom_sensitivity,
+//         pixels_per_line,
+//         ..
+//     } = *controller;
 
-    if !enabled {
-        return;
-    }
+//     if !enabled {
+//         return;
+//     }
 
-    let mut cursor_delta = Vec2::ZERO;
-    for event in mouse_motion_events.iter() {
-        cursor_delta += event.delta;
-    }
+//     let mut cursor_delta = Vec2::ZERO;
+//     for event in mouse_motion_events.iter() {
+//         cursor_delta += event.delta;
+//     }
 
-    if mouse_buttons.pressed(MouseButton::Left) {
-        events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
-    }
+//     if mouse_buttons.pressed(MouseButton::Left) {
+//         events.send(ControlEvent::Orbit(mouse_rotate_sensitivity * cursor_delta));
+//     }
 
-    if mouse_buttons.pressed(MouseButton::Right) {
-        events.send(ControlEvent::TranslateTarget(
-            mouse_translate_sensitivity * cursor_delta,
-        ));
-    }
+//     if mouse_buttons.pressed(MouseButton::Right) {
+//         events.send(ControlEvent::TranslateTarget(
+//             mouse_translate_sensitivity * cursor_delta,
+//         ));
+//     }
 
-    let mut scalar = 1.0;
-    for event in mouse_wheel_reader.iter() {
-        // scale the event magnitude per pixel or per line
-        let scroll_amount = match event.unit {
-            MouseScrollUnit::Line => event.y,
-            MouseScrollUnit::Pixel => event.y / pixels_per_line,
-        };
-        scalar *= 1.0 - scroll_amount * mouse_wheel_zoom_sensitivity;
-    }
-    events.send(ControlEvent::Zoom(scalar));
-}
+//     let mut scalar = 1.0;
+//     for event in mouse_wheel_reader.iter() {
+//         // scale the event magnitude per pixel or per line
+//         let scroll_amount = match event.unit {
+//             MouseScrollUnit::Line => event.y,
+//             MouseScrollUnit::Pixel => event.y / pixels_per_line,
+//         };
+//         scalar *= 1.0 - scroll_amount * mouse_wheel_zoom_sensitivity;
+//     }
+//     events.send(ControlEvent::Zoom(scalar));
+// }
 
-pub fn control_directional_light(
-    time: Res<Time>,
-    mut cursor: EventReader<CursorMoved>,
-    keys: Res<Input<KeyCode>>,
-    mut queries: ParamSet<(
-        Query<&mut Transform, With<DirectionalLight>>,
-        Query<&mut RayCastSource<RaycastSet>>,
-        Query<&Intersection<RaycastSet>>,
-    )>,
-    mut target: Local<Vec3>,
-) {
-    let cursor_position = match cursor.iter().last() {
-        Some(cursor_moved) => cursor_moved.position,
-        None => return,
-    };
+// pub fn control_directional_light(
+//     time: Res<Time>,
+//     mut cursor: EventReader<CursorMoved>,
+//     keys: Res<Input<KeyCode>>,
+//     mut queries: ParamSet<(
+//         Query<&mut Transform, With<DirectionalLight>>,
+//         Query<&mut RayCastSource<RaycastSet>>,
+//         Query<&Intersection<RaycastSet>>,
+//     )>,
+//     mut target: Local<Vec3>,
+// ) {
+//     let cursor_position = match cursor.iter().last() {
+//         Some(cursor_moved) => cursor_moved.position,
+//         None => return,
+//     };
 
-    for mut pick_source in &mut queries.p1() {
-        pick_source.cast_method = RayCastMethod::Screenspace(cursor_position);
-    }
+//     for mut pick_source in &mut queries.p1() {
+//         pick_source.cast_method = RayCastMethod::Screenspace(cursor_position);
+//     }
 
-    if let Ok(intersection) = queries.p2().get_single() {
-        if let Some(position) = intersection.position() {
-            *target = target.lerp(*position, 1.0 - (-10.0 * time.delta_seconds()).exp());
-        }
-    }
+//     if let Ok(intersection) = queries.p2().get_single() {
+//         if let Some(position) = intersection.position() {
+//             *target = target.lerp(*position, 1.0 - (-10.0 * time.delta_seconds()).exp());
+//         }
+//     }
 
-    if keys.pressed(KeyCode::LShift) {
-        if let Ok(mut transform) = queries.p0().get_single_mut() {
-            transform.look_at(*target, Vec3::Z);
-        }
-    }
-}
+//     if keys.pressed(KeyCode::LShift) {
+//         if let Ok(mut transform) = queries.p0().get_single_mut() {
+//             transform.look_at(*target, Vec3::Z);
+//         }
+//     }
+// }
